42f749f66a97901278c8cd8b4289a39e5f248745
Rian Santos <riansantos.dev@gmail.com>
2024-02-03 16:39:27 -0300

    Add Reveal

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+// MENU MOBILE
+
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
@@ -14,3 +16,53 @@ menu.addEventListener('click', () =>{
 overlay.addEventListener('click', () =>{
     menu.classList.remove('abrir-menu')
 })
+
+// REVEAL
+
+window.revelar = ScrollReveal({reset:true})
+
+// TOPO DO SITE
+
+revelar.reveal('.efeito-txt-topo', 
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+// TITULOS DAS SEÇÕES
+
+revelar.reveal('.titulo', 
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+// ESPECIALIDADES
+
+revelar.reveal('.efeito-especialidade1', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+revelar.reveal('.efeito-especialidade2', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+revelar.reveal('.efeito-especialidade3', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+
