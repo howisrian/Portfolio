9f34714cbb921eb3752b68c754ba4730edcb38d2
Rian Santos <116415276+howisrian@users.noreply.github.com>
2024-07-26 12:21:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.post('/enviar', async (req, res) => {
             port: 587,
             secure: false,
             auth: {
-                user: 'contato.riansantos@outlook.com',
-                pass: 'Cudegorila69'
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
             }
         });
 
